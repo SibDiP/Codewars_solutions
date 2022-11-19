b92beea812352f4b78ad1f9cb33e39216d51c680
SibDiP <tskdip@gmail.com>
2022-11-19 22:20:24 +0600

    Add a solution to the 5_kyu_Maximum_subarray_sum problem using Kadane's algorithm | and fix description of bruteforce solution

---
+++ 5_kyu_Maximum_subarray_sum_#Kadane’s_Algorithm.py
@@ -0,0 +1,27 @@
+# Maximum subarray sum (5 kyu) | Kadane’s Algorithm | O(n)
+# https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c/train/python
+#
+# Instructions:
+# The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or
+# list of integers:
+# max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]) # should be 6: [4, -1, 2, 1]
+#
+# Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array.
+# If the list is made up of only negative numbers, return 0 instead.
+#
+# Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid
+# sublist/subarray.
+
+def max_sequence(arr):
+    max_so_far = 0
+    max_ending_here = 0
+
+    for num in arr:
+        max_ending_here += num
+        max_ending_here = max(max_ending_here, 0)
+        max_so_far = max(max_so_far, max_ending_here)
+
+    return max_so_far
+
+
+print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
--- 5_kyu_Maximum_subarray_sum_#bruteforce.py
+++ 5_kyu_Maximum_subarray_sum_#bruteforce.py
@@ -1,4 +1,4 @@
-# Maximum subarray sum (5 kyu) | My bruteforce algorithm
+# Maximum subarray sum (5 kyu) | My bruteforce algorithm | O(n^3)
 # https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c/train/python
 #
 # Instructions:
