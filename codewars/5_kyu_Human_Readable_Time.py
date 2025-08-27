b0b1c309d3d8ea9c695a90a6840f5c81fc37a3d2
Dmitrii Pivnev <tskdip@gmail.com>
2025-08-27 18:39:36 +0700

    Add new answers to 5_kyu_Human_Readable_Time.py

--- codewars/5_kyu_Human_Readable_Time.py
+++ codewars/5_kyu_Human_Readable_Time.py
@@ -14,14 +14,39 @@ You can find some examples in the test fixtures.
 DATE TIME MATHEMATICS ALGORITHMS
 """
 
-# f-string oneliner
+# 1. f-string oneliner
 
 def make_readable(seconds):
     return f"""{str(seconds // 3600).zfill(2)}:{str((seconds % 3600 - seconds
         % 60) // 60).zfill(2)}:{str(seconds % 60).zfill(2)}"""
 
 
-# Better f-string oneliner with digit formatting {nuber:dd}
+# 2. Better f-string oneliner with digit formatting {nuber:dd}
 
 def make_readable(seconds):
     return f"{seconds // 3600:02}:{seconds % 3600 // 60:02}:{seconds % 60:02}"
+
+# 3. dict, direct calculations, f-string
+def make_readable(seconds):
+    time_dict = {'H': '00', 'M': '00', 'S': '00'}
+    time = seconds
+
+    hours = time // 3600
+    min = (time - hours*3600) // 60
+    sec = (time - hours*3600 - min*60)
+
+    return(f"{hours:02}:{min:02}:{sec:02}")
+
+# 4. divmod(), constant, list, for-loop, ":".join()
+def make_readable(seconds: int) -> str:
+    HMS = (('hour', 3600),
+           ('minutes', 60),
+           ('seconds', 1))
+    parts = []
+    reminder = seconds
+
+    for _, measure_value in HMS:
+        result, reminder = divmod(reminder, measure_value)
+        parts.append(f'{result:02}')
+
+    return ":".join(parts)
